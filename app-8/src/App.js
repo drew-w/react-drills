469b471e357b255d1152c723613bc7a7eb81885f
Drew Woodmansee <eesnamdoow@gmail.com>
2021-01-21 21:00:46 -0700

    finished problem 8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      ken: ''
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://secure-hamlet-19722.herokuapp.com/api/v1/characters/Ken/').then(response => {
+      this.setState({
+        ken: response.data,
+      })
+    })
+  }
+
+  render() {
+
+    let kenObj = this.state.ken[0]
+    if (kenObj) {
+      return (
+        <div className='App'>
+          <h1>Name: {kenObj.name}</h1>
+          <img src={kenObj.head_shot} alt='Ken Masters' />
+          <h1>Universe: {kenObj.universe}</h1>
+        </div>
+      )
+    }
+    else {
+      return <div></div>
+    }
+  }
 }
 
 export default App;
